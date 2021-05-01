db7b7c37ea16d19288776469096ff9d2b0ca2ac1
HEIDI-ALVARADO <hlalvarados@unadvirtual.edu.co>
2021-05-01 12:54:28 -0500

    Scripts php para creacion de base de datos y creacion de tabla

---
+++ page/crear_bd.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "12345678";
+
+// Crear conexion
+$conn = new mysqli($servername, $username, $password);
+
+// Validar conexion
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Crear base de datos
+$sql = "CREATE DATABASE IF NOT EXISTS `bdunad33` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
+if ($conn->query($sql) === TRUE) {
+  echo "OK";
+} else {
+    echo "Error creando base de datos: " . $conn->error;
+}
+
+// Cerrar conexion
+$conn->close();
+?>
---
+++ page/crear_tabla.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "12345678";
+$dbname = "bdunad33";
+
+// Crear conexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Validar conexion
+if ($conn->connect_error) {
+  die("Conexion fallida: " . $conn->connect_error);
+}
+
+// sql para crear tabla
+$sql = "CREATE TABLE `bdunad33`.`tabla33` (
+  `product_code` varchar(10) NOT NULL PRIMARY KEY COMMENT 'Codigo del producto',
+  `product_name` varchar(255) NOT NULL COMMENT 'Nombre del producto',
+  `product_brand` varchar(255) NOT NULL COMMENT 'Marca del producto',
+  `product_price` decimal(10,2) NOT NULL COMMENT 'Precio del producto',
+  `product_quantity` int(11) NOT NULL COMMENT 'Cantidad'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tabla de productos'";
+
+// Ejecucion de sentencia sql
+if ($conn->query($sql) === TRUE) {
+  echo "OK";
+} else {
+  echo "Error creando tabla: " . $conn->error;
+}
+
+// Cerrar conexion
+$conn->close();
+?>
