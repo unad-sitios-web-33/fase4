e6dabb8980a03ebc2f8415c6e8cb385ed7738095
silvermiguel96 <silvermiguel96@gmail.com>
2021-05-05 09:22:14 -0500

    crear servicios he integrarlos

--- page/backup_db.php
+++ page/backup_db.php
@@ -18,8 +18,20 @@ if ($conn->connect_error) {
 $dump = "mysqldump -h$servername -u$username -p$password --opt  $dbname > $salida_sql";
 
 system($dump, $uotput);
-if(!$uotput){
-    echo "Backup generado con exito";
+
+$zip = new ZipArchive();
+
+$salida_zip = $dbname.'_'.$fecha.'.zip';
+
+if($zip->open($salida_zip, ZIPARCHIVE::CREATE) === true){
+
+  $zip->addFile($salida_sql);
+  $zip->close();
+  unlink($salida_sql);
+
+  header("Location: http://localhost/fase4/page/".$salida_zip);
+  
+
 } else {
     echo "Error generando Backup";
 }
